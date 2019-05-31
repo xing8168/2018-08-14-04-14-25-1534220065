6f8471cc08f512800a0cf5b387141eae84a69a84
lost <xing8168@sina.cn>
2019-05-31 16:00:24 +0800

    wrote main one time

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main() {
+module.exports = function main(n) {
     console.log("Debug Info");
+    var num = ['._.\n|.|\n|_|', '...\n..|\n..|', '._.\n._|\n|_.',
+	       '._.\n._|\n._|', '...\n|_|\n..|', '._.\n|_.\n._|',
+	       '._.\n|_.\n|_|', '._.\n..|\n..|', '._.\n|_|\n|_|',
+	       '._.\n|_|\n..|'];
+    var arr = [];
+    while(n != 0){
+	int x = n %10;
+	arr.push(n);	
+	n /= 10;
+    }
+    for(int i = arr.length-1; i > 0; i--)
+	console.log(num[arr[i] - 1]);
     return 'Hello World!';
 };
